857b59851ee4bf6bf32ee2b975d5c41a75b77807
MMR <rylander86@hotmail.com>
2013-05-05 22:21:19 +0200

    Recording.
    Added recording functionality that doesn't turn off the sensors when
    screen is turned off. Has a periodical bug but it hasn't appeared during
    a recording.

--- LC10/src/com/example/lc10/MainActivity.java
+++ LC10/src/com/example/lc10/MainActivity.java
@@ -4,48 +4,82 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.location.Location;
-import android.location.LocationListener;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
+import android.widget.TextView;
 
 public class MainActivity extends FragmentActivity implements SensorEventListener {
 	
-	private static Marker marker;
-	private SensorManager sensorManager;
-	private Sensor accelerometer;
+	public SensorManager sensorManager;
+	public Sensor accelerometer;
 	private boolean isRecording = false;
 	private long elapsedTime = 0;
+	private File workingFile;
+	private Intent sensorIntent;
+	private final Handler mhandler = new Handler();
+	private TextView message;
+	private WakeLock wakeLock;
+	
+	private static int numberOfDatapoints = 0;
+	
+	
+	public BroadcastReceiver receiver = new BroadcastReceiver() {
+		
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			if(!intent.getAction().equals(Intent.ACTION_SCREEN_OFF)){
+				return;
+			}
+			
+			Runnable runnable = new Runnable() {
+
+				@Override
+				public void run() {
+					unregisterListener();
+					registerListener();						
+				}				
+			};
+			
+			new Handler().postDelayed(runnable, 500);
+		}
+	};
+	
+	private void registerListener(){
+		sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+	}
+	
+	private void unregisterListener(){
+		sensorManager.unregisterListener(this);
+	}
 	
 
 	@Override
@@ -53,14 +87,58 @@ public class MainActivity extends FragmentActivity implements SensorEventListene
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		message = (TextView)findViewById(R.id.textView1);
+		sensorIntent = new Intent(this, SensorService.class);
+		PowerManager pm = (PowerManager)getSystemService(POWER_SERVICE);
+		wakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "LC10");	
+			
+		
 		Button startButton = (Button)findViewById(R.id.startbutton);
 		startButton.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View arg0) {
-				isRecording = true;
-				elapsedTime = System.currentTimeMillis();
 				
+				Post("Recording started.");
+								
+				try
+				{				
+					DateFormat df = new SimpleDateFormat("dd_MM_yy");
+					String formattedDate = df.format(new Date());				
+					
+					File path = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/AcRecogData");
+					if(!path.exists())
+						path.mkdir();
+					
+					File filename = new File(path.getAbsolutePath() + "/" + formattedDate + ".csv");
+					if(filename.exists()){
+						File[] files = path.listFiles();
+						int highestIndex = 0;
+						for(File f : files){
+							if(f.getAbsolutePath().contains(formattedDate) && f.getAbsolutePath().contains("-")){
+								String[] splits = f.getName().split("-");
+								String index = splits[1].split("\\.")[0];						
+								
+								if(Integer.parseInt(index) > highestIndex)
+									highestIndex = Integer.parseInt(index);								
+							}
+						}
+						
+						filename = new File(filename.getAbsolutePath().replace(".csv", "-" + String.valueOf(highestIndex + 1) + ".csv"));
+					}
+					
+					workingFile = new File(filename.getAbsolutePath());				
+					
+					wakeLock.acquire();
+					isRecording = true;
+					elapsedTime = System.currentTimeMillis();
+					
+//					sensorIntent.putExtra("filename", filename.getAbsolutePath());
+//					startService(sensorIntent);
+				}
+				catch(Exception e){
+					Post(e.getMessage());
+				}
 			}
 		});
 		
@@ -71,31 +149,80 @@ public class MainActivity extends FragmentActivity implements SensorEventListene
 			public void onClick(View v) {
 				isRecording = false;
 				elapsedTime = 0;
+				workingFile = null;
+				wakeLock.release();
+				Post("Recording stopped. Datapoints: " + numberOfDatapoints);
+				numberOfDatapoints = 0;
+				//stopService(sensorIntent);
 				
 			}
 		});
+		
+		
+		Button uploadButton = (Button)findViewById(R.id.uploadbutton);
+		uploadButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				HttpClient client = new DefaultHttpClient();
+				HttpPost post = new HttpPost("http://192.168.1.33:8888/activitytracker");
+				// TODO: Upload .csv file
+
 				
-//		LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);		
-//		boolean isGpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-//		boolean isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-//		
-//		if(isGpsEnabled && isNetworkEnabled){
-//			locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 100, 1, locListener);			
-//		}
+			}
+		});
+		
+		registerReceiver(receiver, new IntentFilter(Intent.ACTION_SCREEN_OFF));
 		
 		sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-		accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);	
+		accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
 
+	}
+	
+	@Override
+	public void onSensorChanged(SensorEvent event) {		
+		if(isRecording){
+			long delta = System.currentTimeMillis();
+			if(elapsedTime != 0 && (delta - elapsedTime) >= 50){ 
+				elapsedTime = delta;
+				numberOfDatapoints++;
+				float[] v = event.values;
+				
+				Date d = new Date();
+				
+				try {		
+
+					FileWriter fw = new FileWriter(workingFile, true);
+					BufferedWriter out = new BufferedWriter(fw);
+					out.write(d.toString() + ";" + v[0] + ";" + v[1] + ";" + v[2] + "\n");
+					out.close();
+					
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					Post(e.getMessage());
+				}			
+			}
+		}
+	}
+	
+	@Override
+	protected void onDestroy(){
+		super.onDestroy();
+		wakeLock.release();
+		stopService(sensorIntent);
 	}
 	
 	protected void onResume(){
 		super.onResume();
-		sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+		wakeLock.acquire();
+		sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
 	}
 	
 	protected void onPause(){
 		super.onPause();
-		sensorManager.unregisterListener(this);
+		wakeLock.release();
+		sensorManager.unregisterListener(this);		
 	}
 
 	@Override
@@ -111,35 +238,21 @@ public class MainActivity extends FragmentActivity implements SensorEventListene
 		// TODO Auto-generated method stub
 		
 	}
+	
 
-	@Override
-	public void onSensorChanged(SensorEvent event) {
-		
-		if(isRecording){
-			long delta = System.currentTimeMillis();
-			if(elapsedTime != 0 && (delta - elapsedTime) > 50){						
-				float[] v = event.values;
-				
-				Date d = new Date();
-				
-				try {
-					
-					File f = Environment.getExternalStorageDirectory();
-					FileWriter fw = new FileWriter(f.getAbsolutePath() + "/out.csv");
-					BufferedWriter out = new BufferedWriter(fw);
-					out.write(d.toString() + "," + v[0] + "," + v[1] + "," + v[2]);
-					out.close();
-					
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} 
-				
+
+	
+    public void Post(final String msg){
+  		mhandler.post(new Runnable(){
+
+			@Override
+			public void run() {
+				message.setText(msg);
 				
 			}
-				
-		}
-	}
+  			
+  		});
+    }
 
 	private void SendAccelerometerData(float[] v, String username) {
 		HttpClient client = new DefaultHttpClient();
